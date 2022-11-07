53f11b71451c1d317bae929951e535cac10630d7
Olesia Andreiko <olesikmail@gmail.com>
2022-11-08 01:28:10 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,41 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+const TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem(
-      'videoplayer-current-time',
-      JSON.stringify(data.seconds)
-    );
-  }, 1000)
-);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(
-    localStorage.getItem('videoplayer-current-time')
-    );
+try {
+  const playbackTime = Number(localStorage.getItem(TIME))
+  player.setCurrentTime(playbackTime)  
+} catch (error) {
+  console.log(error.name)
+}
+
+function onPlay(data){
+  const currentTime = data.seconds
+  localStorage.setItem(TIME, currentTime)
+}
+
+
+
+
+// player.setCurrentTime(savedTime).then(function(seconds) {
+//   // seconds = Number(localStorage.getItem(TIME))
+//   console.log(seconds)
+//   // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//   switch (error.name) {
+//       case 'RangeError':
+//         !seconds           
+//          // the time was less than 0 or greater than the video’s duration
+//           break;
+//       default:
+//         seconds === 0
+//           // some other error occurred
+//           break;
+//   }
+// });
+
